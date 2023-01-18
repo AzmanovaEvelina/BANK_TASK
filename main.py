099ca6b85f7ca14463fbb52abf5a19e01f071a9e
AzmanovaEvelina <azmanova.evelina.r@gmail.com>
2023-01-18 18:08:59 +0500

    task_4_1 update name txt with answers + task_4_2

--- main.py
+++ main.py
@@ -21,7 +21,57 @@ def arithmetic_1():
             return a, b, c, e, d, res
 
 
-class id0401001(Scene):
+# class id0401001(Scene):
+#
+#     def construct(self):
+#
+#         self.camera.background_color = DARKER_GRAY
+#
+#         s1 = Text("МАТЕМАТИКА", font="montserrat", weight=HEAVY).scale(0.4).to_corner(DR, buff=1).shift(0.6 * DOWN).set_color(BLUE_A)
+#         s2 = Text("с Никитой Евгеньевичем", font="montserrat", weight=THIN).scale(0.3).next_to(s1, DOWN, buff=0.05, aligned_edge=LEFT).set_color(BLUE_A)
+#         s = VGroup(s1,s2).shift(2*UP)
+#         s3 = Text("Найдите значение выражения", font="montserrat", weight=HEAVY).scale(0.7).to_corner(UL, buff=1).shift(1.4*DOWN).shift(0.4*LEFT)
+#
+#         f = open('answer_4_1.txt','w')
+#
+#         i = 0
+#         while i < 10:
+#             a, b, c, e, d, res = arithmetic_1()
+#             if(d < 0):
+#                 d = d * -1
+#                 t = MathTex(a, "{", b, "\\over", c, "}", " : ", "\\left(","-","{", d, "\\over", e, "}","\\right)", " = ", font_size = 60)
+#             else:
+#                 t = MathTex( a, "{", b, "\\over", c, "}", " : ", "{", d, "\\over", e, "}", " = ", font_size = 60)
+#
+#             if (res - int(res)) == 0:
+#                 res = round(res)
+#             f.write(f'{res}\n')
+#             self.add(t, s, s3)
+#             self.wait(1 / 60)
+#             self.clear()
+#             i += 1
+#
+#         f.close()
+#
+
+def arithmetic_2():
+    bool = True
+    while bool:
+        a = random.randint(1, 400)
+        b = random.randint(1, 100)
+        cc = random.choice([-1, 1])
+        if (cc == 1):
+            c = a + b
+            res = a-b
+        else:
+            c = a - b
+            res = a + b
+
+        if (a > b):
+            bool = False
+            return a, b, c, res
+
+class id0401002(Scene):
 
     def construct(self):
 
@@ -32,19 +82,14 @@ class id0401001(Scene):
         s = VGroup(s1,s2).shift(2*UP)
         s3 = Text("Найдите значение выражения", font="montserrat", weight=HEAVY).scale(0.7).to_corner(UL, buff=1).shift(1.4*DOWN).shift(0.4*LEFT)
 
-        f = open('answer.txt','w')
+        f = open('answer_4_2.txt','w')
 
         i = 0
         while i < 10:
-            a, b, c, e, d, res = arithmetic_1()
-            if(d < 0):
-                d = d * -1
-                t = MathTex(a, "{", b, "\\over", c, "}", " : ", "\\left(","-","{", d, "\\over", e, "}","\\right)", " = ", font_size = 60)
-            else:
-                t = MathTex( a, "{", b, "\\over", c, "}", " : ", "{", d, "\\over", e, "}", " = ", font_size = 60)
+            a, b, c, res = arithmetic_2()
 
-            if (res - int(res)) == 0:
-                res = round(res)
+            t = MathTex("\\left(",a,"^2","-",b,"^2", "\\right)", " : ", c, " = ",font_size=60)
+
             f.write(f'{res}\n')
             self.add(t, s, s3)
             self.wait(1 / 60)
