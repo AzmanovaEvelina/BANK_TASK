5b792db86273c6d4118d0bfe318779ca72c602c5
AzmanovaEvelina <azmanova.evelina.r@gmail.com>
2023-01-18 22:13:57 +0500

    task_4_3

--- main.py
+++ main.py
@@ -88,3 +88,67 @@ class id0401002(Scene):
             i += 1
 
         f.close()
+
+def arithmetic_3_1():
+    bool = True
+    while bool:
+        number_1 = random.randint(101, 888)
+        number_2 = random.randint(101, 888)
+        if (number_1 % 10 != 0 and number_2 % 10 != 0):
+            bool = False
+    a, c = arithmetic_3_2(number_1)
+    b, d = arithmetic_3_2(number_2)
+
+    res = round((round((a*b),6)/ round((c*d),6)), 4)
+
+    if (res - int(res)) == 0:
+        res = round(res)
+
+    return a, b, c, d, res
+def arithmetic_3_2(number):
+    rnd = 0
+    rnd_1 = 0
+    while (rnd == rnd_1):
+        rnd = random.choice([1, 2, 3])
+        rnd_1 = random.choice([1, 2, 3])
+
+    if rnd == 1:
+        number_1 = number / 10
+        if rnd_1 == 2:
+            number_2 = number / 100
+        else:
+            number_2 = number / 1000
+    if rnd == 2:
+        number_1 = number / 100
+        if rnd_1 == 1:
+            number_2 = number / 10
+        else:
+            number_2 = number / 1000
+    if rnd == 3:
+        number_1 = number / 1000
+        if rnd_1 == 2:
+            number_2 = number / 100
+        else:
+            number_2 = number / 10
+    return number_1, number_2
+class id0401003(Scene):
+
+    def construct(self):
+
+        self.camera.background_color = DARKER_GRAY
+
+        f = open('answer_4_3.txt','w')
+
+        i = 0
+        while i < 10:
+            a, b, c, d, res = arithmetic_3_1()
+
+            t = MathTex("{", a, " \\cdot ", b, "\\over", c, " \\cdot ", d, "}", " = ", font_size=60)
+
+            f.write(f'{res}\n')
+            self.add(t, s, s3)
+            self.wait(1 / 60)
+            self.clear()
+            i += 1
+
+        f.close()
--- test_TASK.py
+++ test_TASK.py
@@ -1,24 +1,49 @@
 import random
-
-def irrational_1():
+def arithmetic_3_1():
     bool = True
     while bool:
-        a = random.randint(1, 10) * random.choice([-1, 1])
-        b = random.randint(1, 15) * random.choice([-1, 1])
-        c = random.randint(1, 30)
-
-        d = random.randint(1, 15) * random.choice([-1, 1])
-        e = c * random.choice([1, 5])
-
-        res = ((a*c+b)/c)*(e/d)
-
-        if (res - int(res * 100) / 100) == 0:
+        number_1 = random.randint(101, 888)
+        number_2 = random.randint(101, 888)
+        if (number_1 % 10 != 0 and number_2 % 10 != 0):
             bool = False
-            return a, b, c, e, d, res
+    a, c = arithmetic_3_2(number_1)
+    b, d = arithmetic_3_2(number_2)
 
+    res = round((round((a*b),6)/ round((c*d),6)), 4)
+
+    if (res - int(res)) == 0:
+        res = round(res)
+
+    return a, b, c, d, res
+def arithmetic_3_2(number):
+    rnd = 0
+    rnd_1 = 0
+    while (rnd == rnd_1):
+        rnd = random.choice([1, 2, 3])
+        rnd_1 = random.choice([1, 2, 3])
+
+    if rnd == 1:
+        number_1 = number / 10
+        if rnd_1 == 2:
+            number_2 = number / 100
+        else:
+            number_2 = number / 1000
+    if rnd == 2:
+        number_1 = number / 100
+        if rnd_1 == 1:
+            number_2 = number / 10
+        else:
+            number_2 = number / 1000
+    if rnd == 3:
+        number_1 = number / 1000
+        if rnd_1 == 2:
+            number_2 = number / 100
+        else:
+            number_2 = number / 10
+    return number_1, number_2
 
 i = 0
-while i < 11:
-    a, b, c, e, d, res = irrational_1()
-    print(f'{a} {b}/{c} : {d}/{e} = {res}')
+while i < 100:
+    a, b, c, d, res = arithmetic_3_1()
+    print (a, b, c, d, res)
     i += 1
