116b3c7d78db73d873ee0f68fd8cebe7d572b4c5
AzmanovaEvelina <azmanova.evelina.r@gmail.com>
2023-01-20 16:39:58 +0500

    fix task_4_3

--- main.py
+++ main.py
@@ -109,28 +109,13 @@ def arithmetic_3_2(number):
     rnd = 0
     rnd_1 = 0
     while (rnd == rnd_1):
-        rnd = random.choice([1, 2, 3])
-        rnd_1 = random.choice([1, 2, 3])
+        rnd = random.choice([10, 100, 1000])
+        rnd_1 = random.choice([10, 100, 1000])
 
-    if rnd == 1:
-        number_1 = number / 10
-        if rnd_1 == 2:
-            number_2 = number / 100
-        else:
-            number_2 = number / 1000
-    if rnd == 2:
-        number_1 = number / 100
-        if rnd_1 == 1:
-            number_2 = number / 10
-        else:
-            number_2 = number / 1000
-    if rnd == 3:
-        number_1 = number / 1000
-        if rnd_1 == 2:
-            number_2 = number / 100
-        else:
-            number_2 = number / 10
+    number_1 = number / rnd
+    number_2 = number / rnd_1
     return number_1, number_2
+
 class id0401003(Scene):
 
     def construct(self):
@@ -140,7 +125,7 @@ class id0401003(Scene):
         f = open('answer_4_3.txt','w')
 
         i = 0
-        while i < 10:
+        while i < 3:
             a, b, c, d, res = arithmetic_3_1()
 
             t = MathTex("{", a, " \\cdot ", b, "\\over", c, " \\cdot ", d, "}", " = ", font_size=60)
