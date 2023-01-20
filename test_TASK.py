f026ff578fb630e4a0e4c38ade93d748be118db8
AzmanovaEvelina <azmanova.evelina.r@gmail.com>
2023-01-20 16:44:03 +0500

    fix task_4_3

--- test_TASK.py
+++ test_TASK.py
@@ -19,31 +19,14 @@ def arithmetic_3_2(number):
     rnd = 0
     rnd_1 = 0
     while (rnd == rnd_1):
-        rnd = random.choice([1, 2, 3])
-        rnd_1 = random.choice([1, 2, 3])
-
-    if rnd == 1:
-        number_1 = number / 10
-        if rnd_1 == 2:
-            number_2 = number / 100
-        else:
-            number_2 = number / 1000
-    if rnd == 2:
-        number_1 = number / 100
-        if rnd_1 == 1:
-            number_2 = number / 10
-        else:
-            number_2 = number / 1000
-    if rnd == 3:
-        number_1 = number / 1000
-        if rnd_1 == 2:
-            number_2 = number / 100
-        else:
-            number_2 = number / 10
+        rnd = random.choice([10, 100, 1000])
+        rnd_1 = random.choice([10, 100, 1000])
+    #number_2 = number
+    number_1 = number / rnd
+    number_2 =  number/rnd_1
     return number_1, number_2
-
 i = 0
-while i < 100:
+while i < 50:
     a, b, c, d, res = arithmetic_3_1()
     print (a, b, c, d, res)
     i += 1
